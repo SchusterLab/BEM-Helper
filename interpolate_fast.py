fe8c78696def2f187a3da33e7c4454252337414c
gkoolstra <gkoolstra@wireless-s1-so-150-53-46.uchicago.edu>
2016-05-17 18:36:46 -0500

    Renamed some functions. Added construct_vertices_from_grid

--- interpolate_fast.py
+++ interpolate_fast.py
@@ -1,9 +1,8 @@
 import numpy as np
 import interpolate_slow
+import os
 
-
-
-def construct_vertices(xy , uv):
+def construct_vertices_from_coordinates(xy , uv):
     """
     Preparation method for fast interpolation. This method calculates vertices and weights once, such that
     the interpolation can be done much quicker than e.g. scipy.interpolate.griddata
@@ -20,7 +19,7 @@ def construct_vertices(xy , uv):
     bary = np.einsum('njk,nk->nj', temp[:, :2, :], delta)
     return vertices, np.hstack((bary, 1 - bary.sum(axis=1, keepdims=True)))
 
-def fast_interpolate(values, vertices, weights):
+def interpolate_from_vertices(values, vertices, weights):
     """
     Fast interpolation method (linear), to be executed after construct_vertices
     :param values: Values of the FEM solution at the FEM mesh points
@@ -29,6 +28,32 @@ def fast_interpolate(values, vertices, weights):
     :return: Interpolated values at the BEM mesh points.
     """
     return np.einsum('nj,nj->n', np.take(values, vertices), weights)
+
+def construct_vertices_from_grid(c, filename, physical_group):
+    """
+    Set up for fast interpolation of a single face (physical group). Usually followed by
+    :param c: diagnose.ElementCoordinates instance
+    :param filename: Filename of a single file containing FEM BC data
+    :param physical_group: diagnose.ElementCoordinate.PhysicalGroupx instance. This is instantiated after running
+    running diagnose.ElementCoordinate.get_element_coordinates(...,...)
+    :return: Vertices, Weights, XY points of the BEM mesh, Solution on the FEM mesh
+    """
+    x1_unique, x2_unique = c.get_unique_points(physical_group['x'], physical_group['y'], physical_group['z'])
+    #print x1_unique.shape, x2_unique.shape
+
+    XY_eval = hstack((x1_unique.reshape(x1_unique.shape[0],1), x2_unique.reshape(x2_unique.shape[0],1)))
+    # Then we need the FEM mesh points, make sure we get the right corresponding surface.
+    elements, nodes, elem_solution = interpolate_slow.get_maxwell_boundary_data(filename)
+    xdata, ydata, Udata = interpolate_slow.prepare_for_interpolation(elements, nodes, elem_solution)
+
+    XY_data = hstack((xdata.reshape(xdata.shape[0],1), ydata.reshape(ydata.shape[0],1)))
+    # Evaluate the vertices and weights at the BEM mesh points specified in XY_eval
+    vertices, weights = construct_vertices(XY_data, XY_eval)
+
+    return vertices, weights, XY_eval, Udata
+
+
+
 
 
 
